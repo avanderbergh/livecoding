5992633a5e260659633d560c50efa272aa5b56f6
Adriaan van der Bergh <avanderbergh@gmail.com>
2020-06-10 17:19:27 +0000

    Update

--- VideoCamera/script.js
+++ VideoCamera/script.js
@@ -1,4 +1,7 @@
 const video = document.querySelector("video");
+const oldTimeyButton = document.querySelector("#old-timey-button");
+const hueSlider = document.querySelector("#hue-slider");
+const effectButton = document.querySelector("#effect-button");
 
 navigator.getUserMedia({video: true, audio: false}
     , stream => {
@@ -7,3 +10,26 @@ navigator.getUserMedia({video: true, audio: false}
     }, err => {
         console.error(err);
     });
+
+oldTimeyButton.addEventListener("click", e => {
+    setTimeout(() => {
+        video.style.filter = "sepia(100%)";
+    }, 3000)
+});
+
+hueSlider.addEventListener("change", e=> {
+    console.log(hueSlider.value);
+    video.style.filter = `hue-rotate(${hueSlider.value}deg)`;
+});
+
+const changeHue = hue => {
+    video.style.filter = `hue-rotate(${hue}deg)`;
+    return hue++;
+}
+
+effectButton.addEventListener("click", e => {
+    changeHue(0);
+    while (hue < 360) {
+        changeHue()
+    }
+})
