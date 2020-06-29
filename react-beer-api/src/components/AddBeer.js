5e5ea13178d7937b6ba417750b260bdeb2d8ced3
Adriaan van der Bergh <avanderbergh@gmail.com>
2020-06-29 17:09:55 +0000

    Added Add Beer Component

---
+++ react-beer-api/src/components/AddBeer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AddBeer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+    render() {
+        return (<h1>Add Beer</h1>);
+    }
+}
+
+export default AddBeer;
